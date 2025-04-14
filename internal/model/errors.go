d72fc7c9a24a8847384bedce6c333203143023e8
Inna Maikut <maikutinna@gmail.com>
2025-04-15 01:52:15 +0300

    Remove extra errors

--- internal/model/errors.go
+++ internal/model/errors.go
@@ -3,17 +3,8 @@ package model
 import "errors"
 
 var (
-	ErrEmployeeNotFound      = errors.New("employee not found")
-	ErrWrongEmployeePassword = errors.New("wrong employee password")
-	ErrEmployeeAlreadyExists = errors.New("employee already exists")
-
 	ErrReceptionNotFound      = errors.New("reception not found")
 	ErrReceptionAlreadyExists = errors.New("reception already exists")
 
 	ErrProductNotFound = errors.New("product not found")
-
-	ErrPVZNotFound = errors.New("PVZ not found")
-
-	ErrNotEnoughBalance               = errors.New("not enough balance")
-	ErrSendingCoinsToMyselfNotAllowed = errors.New("sending coins to myself not allowed")
 )
